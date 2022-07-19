50f289f594bcd5d91b83427f01e451f8e05b28b9
Dennis Kibahila <kibahiladennis@gmail.com>
2022-07-19 17:28:06 +0300

    added report functionalities

---
+++ backend/src/main/java/com/Adapter/icare/DHIS2/DHISDomains/ReportValuesSent.java
@@ -0,0 +1,12 @@
+package com.Adapter.icare.DHIS2.DHISDomains;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReportValuesSent {
+    private String dataSetID;
+    private String periodStart;
+    private String periodEnd;
+}
