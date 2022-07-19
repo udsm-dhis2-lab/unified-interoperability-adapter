787955c1064bdb381483e48693f519e5db37bb09
josephatJ <josephatjulius24@gmail.com>
2022-07-19 17:57:05 +0300

    Add ngx dhis2 period filter

--- ui/src/app/components/page/mapping/custom-form/custom-form.component.ts
+++ ui/src/app/components/page/mapping/custom-form/custom-form.component.ts
@@ -1,5 +1,8 @@
 import { DataValueFetchService } from './../../../../services/dataValueFetch/data-value-fetch.service';
-import { DatasetInterface, DataValueFetchInterface } from './../../../../resources/interfaces';
+import {
+  DatasetInterface,
+  DataValueFetchInterface,
+} from './../../../../resources/interfaces';
 import {
   Component,
   OnInit,
@@ -29,9 +32,9 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
   @Input() dataSetDataValues: any;
 
   @Input() isDataEntryLevel: any;
-  
+
   @Input() entryFormType: string;
-  
+
   @Input() sources: SourceInterface[] | undefined;
   @Input() dataset: DatasetInterface | undefined;
 
@@ -44,16 +47,32 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
   query: string | undefined;
   dataValueFetchs: DataValueFetchInterface[] | undefined;
   dataValueFetch: any;
-  
+
+  /**For period filter */
+  periodObject: any;
+  periodFilterConfig: any = {
+    singleSelection: true,
+    emitOnSelection: true,
+    childrenPeriodSortOrder: 'ASC',
+    allowDateRangeSelection: true,
+    allowRelativePeriodSelection: true,
+    allowFixedPeriodSelection: true,
+    hideActionButtons: true,
+    contentHeight: '300px',
+  };
+  selectedPeriodItems: any[] = [];
+  /**
+   * End of period filter
+   */
+
   constructor(
     private dataValueFetchService: DataValueFetchService,
-    private sanitizer?: DomSanitizer, 
+    private sanitizer?: DomSanitizer,
     private elementRef?: ElementRef,
     public dialog?: MatDialog,
     private router?: Router,
     private route?: ActivatedRoute
-  ) 
-  {
+  ) {
     this.entryFormStatusColors = {
       OK: '#b9ffb9',
       WAIT: '#fffe8c',
@@ -87,17 +106,25 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
     }
   }
 
+  onPeriodUpdate(periodObject: any, action: string): void {
+    this.periodObject = periodObject;
+  }
+
   ngAfterViewInit() {
     this.setScriptsOnHtmlContent(
       this.getScriptsContents(this.dataSetFormDesign.formdesignCode)
     );
-    this.getEnabledInputTagsOnHtmlContent(this.sources, this, this.dataSetFormDesign.formdesignCode)
-    
+    this.getEnabledInputTagsOnHtmlContent(
+      this.sources,
+      this,
+      this.dataSetFormDesign.formdesignCode
+    );
+
     // console.log("Dataset We need: ",this.dataset)
   }
 
   getScriptsContents(html: any) {
-    if (html){
+    if (html) {
       const matchedScriptArray = html.match(
         /<script[^>]*>([\w|\W]*)<\/script>/im
       );
@@ -108,7 +135,7 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
               .split(':separator:')
               .filter((content: any) => content.length > 0)
           : [];
-  
+
       return _.filter(scripts, (scriptContent: string) => scriptContent !== '');
     }
     return undefined;
@@ -130,13 +157,17 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
         this.entryFormStatusColors,
         this.isDataEntryLevel,
         scriptsContentsArray,
-        function (entryFormType: any, entryFormStatusColors: any, isDataEntryLevel: any) {
+        function (
+          entryFormType: any,
+          entryFormStatusColors: any,
+          isDataEntryLevel: any
+        ) {
           // Listen for change event
           document.addEventListener(
             'change',
             function (event: any) {
               // If the clicked element doesn't have the right selector, bail
-              console.log("Event: ",event);
+              console.log('Event: ', event);
               if (
                 event.target.matches(
                   '.entryfield, .entryselect, .entrytrueonly, .entryfileresource'
@@ -165,30 +196,31 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
     }
   }
 
-  getEnabledInputTagsOnHtmlContent(sources?: SourceInterface[], thisComponent?: CustomFormComponent, html?: string){
+  getEnabledInputTagsOnHtmlContent(
+    sources?: SourceInterface[],
+    thisComponent?: CustomFormComponent,
+    html?: string
+  ) {
     let parser = new DOMParser();
     const formDesign = parser.parseFromString(html!, 'text/html');
     //Always disable input tags
     let inputElements = formDesign.getElementsByTagName('input');
 
-
-
-
-    //Trying to get data existing after double clicking 
-    
+    //Trying to get data existing after double clicking
 
     //end
 
-    document.addEventListener('click', async function(event: any) {
-      if(event.target.name === 'entryfield'){
+    document.addEventListener('click', async function (event: any) {
+      if (event.target.name === 'entryfield') {
         thisComponent?.openDialog(sources!, event.target.id);
       }
-    })
+    });
   }
 
-  
-  async openDialog(sourcesToChoose: SourceInterface[], elementId: string): Promise<any> {
-      
+  async openDialog(
+    sourcesToChoose: SourceInterface[],
+    elementId: string
+  ): Promise<any> {
     const dataValueFetchObject = {
       dataElementCategoryOptionCombo: elementId,
       sqlQuery: undefined,
@@ -200,7 +232,7 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
       },
     };
 
-   this?.dataValueFetchService
+    this?.dataValueFetchService
       .getSingleDataValueFetch(dataValueFetchObject)
       .subscribe((dataValueFetch) => {
         this.dataValueFetch = dataValueFetch;
@@ -211,7 +243,7 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
     await new Promise((resolve) => setTimeout(resolve, 500));
 
     console.log('Data Value Fetching 2: ', this.dataValueFetch);
-   
+
     const dialogRef = this.dialog?.open(AddQueryComponent, {
       autoFocus: true,
       // disableClose: true,
@@ -221,51 +253,60 @@ export class CustomFormComponent implements OnInit, AfterViewInit {
         query: this.dataValueFetch?.sqlQuery,
         source: this.dataValueFetch?.datasource,
         elementId: elementId,
-        id: this.dataValueFetch?.id
+        id: this.dataValueFetch?.id,
       },
     });
 
     // console.log("Opened: "+ this.dataValueFetch);
 
-    dialogRef?.afterClosed().subscribe(result => {
-      if(result){
-          this.source = result.source;
-          this.query = result.query;
-          // console.log("Source: "+this.source?.type + " Query:" + this.query + ' Dataset: ' + this.dataset?.displayName+ ' Element ID: ' + result.elementId)
-          this.addDataValueFetch(this.source?.id, this.query, this.dataset?.id, result.elementId, result.id);
-          
-      } 
-      else{
-        console.log("No data found");  
+    dialogRef?.afterClosed().subscribe((result) => {
+      if (result) {
+        this.source = result.source;
+        this.query = result.query;
+        // console.log("Source: "+this.source?.type + " Query:" + this.query + ' Dataset: ' + this.dataset?.displayName+ ' Element ID: ' + result.elementId)
+        this.addDataValueFetch(
+          this.source?.id,
+          this.query,
+          this.dataset?.id,
+          result.elementId,
+          result.id
+        );
+      } else {
+        console.log('No data found');
         this.source = undefined;
         this.query = undefined;
-        
-      }  
+      }
 
       this.router!.routeReuseStrategy.shouldReuseRoute = () => false;
       this.router!.onSameUrlNavigation = 'reload';
       // this.router!.navigate(['/'], {
       //   relativeTo: this.route
       // })
-
     });
   }
 
-  addDataValueFetch(dataSource?: number, query?: string, dataset?: string,  elementCombo?: string, id?: any){
-    
+  addDataValueFetch(
+    dataSource?: number,
+    query?: string,
+    dataset?: string,
+    elementCombo?: string,
+    id?: any
+  ) {
     const dataValueFetchObject = {
-        id: id,
-        dataElementCategoryOptionCombo: elementCombo,
-        sqlQuery: query,
-        datasets: {
-            id: dataset,
-        },
-        datasource: {
-            id: dataSource,
-        },
-      }  
-    this.dataValueFetchService.addDataValueFetch(dataValueFetchObject).subscribe((dataValueFetch) => (this.dataValueFetchs?.push(dataValueFetch)));
-
+      id: id,
+      dataElementCategoryOptionCombo: elementCombo,
+      sqlQuery: query,
+      datasets: {
+        id: dataset,
+      },
+      datasource: {
+        id: dataSource,
+      },
+    };
+    this.dataValueFetchService
+      .addDataValueFetch(dataValueFetchObject)
+      .subscribe((dataValueFetch) =>
+        this.dataValueFetchs?.push(dataValueFetch)
+      );
   }
-
 }
