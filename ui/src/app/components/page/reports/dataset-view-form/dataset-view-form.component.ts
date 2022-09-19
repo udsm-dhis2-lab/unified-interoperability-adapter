c1b7ee492921c97680c58f10b5a900ed919a5ec6
josephatJ <josephatjulius24@gmail.com>
2022-09-19 17:48:29 +0300

    Add support to download to excel

--- ui/src/app/components/page/reports/dataset-view-form/dataset-view-form.component.ts
+++ ui/src/app/components/page/reports/dataset-view-form/dataset-view-form.component.ts
@@ -149,4 +149,38 @@ export class DatasetViewFormComponent implements OnInit, AfterViewInit {
       ?.sendReport(this.sendingObject)
       .subscribe((values) => console.log(values));
   }
+
+  onDownloadToExcel(event: Event, id: string): void {
+    event.stopPropagation();
+    const fileName = 'Report_';
+    event.stopPropagation();
+    const htmlTable = document.getElementById(id)?.outerHTML;
+    // const htmlTable = window.document.getElementById(id).outerHTML;
+    if (htmlTable) {
+      const uri = 'data:application/vnd.ms-excel;base64,',
+        template =
+          '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:' +
+          'office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook>' +
+          '<x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' +
+          '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
+          '</head><body><table border="1">{table}</table><br /><table border="1">{table}</table></body></html>',
+        base64 = (s: any) => window.btoa(unescape(encodeURIComponent(s))),
+        format = (s: any, c: any) =>
+          s.replace(/{(\w+)}/g, (m: any, p: any) => c[p]);
+
+      const ctx = { worksheet: 'Data', filename: fileName };
+      let str =
+        '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office' +
+        ':excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook>' +
+        '<x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' +
+        '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>';
+      // ctx['div'] = htmlTable;
+
+      str += htmlTable + '</body></html>';
+      const link = document.createElement('a');
+      link.download = fileName + '.xls';
+      link.href = uri + base64(format(str, ctx));
+      link.click();
+    }
+  }
 }
