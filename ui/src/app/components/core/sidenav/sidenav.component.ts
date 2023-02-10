d8b66fa418f238ea53a128c11a19a9803744a899
josephatJ <josephatjulius24@gmail.com>
2023-02-10 10:02:11 +0300

    Fix UI issues

--- ui/src/app/components/core/sidenav/sidenav.component.ts
+++ ui/src/app/components/core/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SidenavComponent implements OnInit {
   @Input() opened?: boolean;
+  @Input() selectedRoute: string | undefined;
 
   showSidenav: boolean = true;
   isAdmin: boolean = false;
@@ -15,28 +16,29 @@ export class SidenavComponent implements OnInit {
 
   back: string = '<';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    // this.selectedRoute =
+    console.log(this.route.snapshot.queryParams);
+    this.route.queryParams.subscribe((params) => {
+      this.param = params['admin'];
 
-    this.route.queryParams
-      .subscribe(params => {
-        this.param = params['admin'];
-
-        if (this.param === "admin") {
-
-          this.isAdmin = true
-
-        }
-
-      });
-
+      if (this.param === 'admin') {
+        this.isAdmin = true;
+      }
+    });
   }
-
-
-
 
   changeIcon() {
     this.showSidenav = !this.showSidenav;
+  }
+
+  onChangeRoute(event: Event, route: string): void {
+    event.stopPropagation();
+    this.selectedRoute = route;
+    this.router.navigate(['/' + this.selectedRoute], {
+      queryParams: { admin: 'admin' },
+    });
   }
 }
