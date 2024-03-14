6373b76a403652ed538ed962b8435f2dae507352
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-03-14 18:01:37 +0300

    Add missing ui reference

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 npm i --legacy-peer-deps
-docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 npm run build
-docker run -w="/app" -v "$(pwd)"/:/app node:16.20.1 bash -c "rm -rf /app/backend/src/main/resources/static && cp -r /app/ui/dist /app/backend/src/main/resources/static"
+docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 npm run build:prod --aot
+docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 bash -c "rm -rf /app/backend/src/main/resources/static && cp -r /app/ui/dist /app/backend/src/main/resources/static"
 docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/backend:/usr/src/omod -w /usr/src/omod maven:3.6.3 mvn package -Dmaven.test.skip=true
 docker build -f Dockerfile  -t udsmdhis2/unified:1.0.0 .
