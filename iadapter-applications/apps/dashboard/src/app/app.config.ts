fb724372750b8728a52904a402930d024171758d
yuzalsif <liteit68@gmail.com>
2024-10-04 15:46:51 +0300

    Add dashboard app in the list of hdu apps

--- iadapter-applications/apps/dashboard/src/app/app.config.ts
+++ iadapter-applications/apps/dashboard/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { antDesignIcons } from './shared/ant-design-icons.constants';
 
+const icons: IconDefinition[] = [...antDesignIcons];
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    { provide: NZ_ICONS, useValue: icons },
   ],
 };
---
+++ iadapter-applications/apps/dashboard/src/app/shared/ant-design-modules.ts
@@ -0,0 +1,13 @@
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+
+export const antDesignModules = [
+  NzLayoutModule,
+  NzMenuModule,
+  NzIconModule,
+  NzPageHeaderModule,
+  NzSpaceModule,
+];
