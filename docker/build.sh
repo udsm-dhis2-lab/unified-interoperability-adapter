82264b9609092fd452b97a66a4dffaea4aa5fed1
josephatJ <josephatjulius24@gmail.com>
2024-10-04 12:02:55 +0300

    Change nx folder name on readme

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
-docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 npm i --legacy-peer-deps
-docker run -w="/app" -v "$(pwd)"/ui:/app node:16.20.1 npm run build
+docker run -w="/app" -v "$(pwd)"/ui:/app node:20.17.0 npm i --legacy-peer-deps
+docker run -w="/app" -v "$(pwd)"/ui:/app node:20.17.0 npm run build
 docker run -w="/app" -v "$(pwd)"/:/app node:16.20.1 bash -c "rm -rf /app/backend/src/main/resources/static && cp -r /app/ui/dist /app/backend/src/main/resources/static"
 docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/backend:/usr/src/omod -w /usr/src/omod maven:3.6.3 mvn package -Dmaven.test.skip=true
 docker build --no-cache -f Dockerfile  -t udsmdhis2/unified:2.0.0 .
