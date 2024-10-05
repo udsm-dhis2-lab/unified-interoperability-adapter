6c0d62201b83e6005a224a793d00bb10c41d8ccc
josephatJ <josephatjulius24@gmail.com>
2024-10-05 17:07:05 +0300

    Fix copying apps to static folder issue

--- docker/build.sh
+++ docker/build.sh
@@ -51,16 +51,21 @@ done
 
 echo "Unzipping completed. Check the $EXTRACT_DIR directory for extracted contents."
 
-docker run --rm -w="/app" -v "$(pwd)":/app node:20.18.0 bash -c "\
-    rm -rf /app/backend/src/main/resources/static && \
-    mkdir -p /app/backend/src/main/resources/static && \
-    for dir in /app/ui-apps/*; do \
-        if [ -d \"$dir\" ]; then \
-            app_name=$(basename \"$dir\"); \
-            cp -r \"$dir\" /app/backend/src/main/resources/static/$app_name/; \
-            echo \"Copied $app_name to /app/backend/src/main/resources/static/$app_name\"; \
-        fi; \
-    done"
+docker run -w="/app" -v "$(pwd)":/app node:20.18.0 bash -c "
+    # Remove existing static content if any
+    rm -rf /app/backend/src/main/resources/static/* && \
+    
+    # Iterate through each directory in the extracted UI apps
+    for dir in /app/ui-apps/*; do
+        if [ -d \"\$dir\" ]; then
+            app_name=\$(basename \"\$dir\")
+            
+            # Copy the directory into the backend static resources directory
+            cp -r \"\$dir\" \"/app/backend/src/main/resources/static/\$app_name\"
+            echo \"Copied \$app_name to /app/backend/src/main/resources/static/\$app_name\"
+        fi
+    done
+"
 
 docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/backend:/usr/src/omod -w /usr/src/omod maven:3.6.3 mvn package -Dmaven.test.skip=true
 docker build --no-cache -f Dockerfile  -t udsmdhis2/unified:2.0.0 .
