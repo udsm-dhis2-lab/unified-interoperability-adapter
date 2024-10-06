8ff68c0467165d26a10231093e559bc4b18342db
yuzalsif <liteit68@gmail.com>
2024-10-06 19:05:26 +0300

    Remove court of arm from login page

--- iadapter-applications/apps/client-management/src/app/modules/deduplication/deduplication.module.ts
+++ iadapter-applications/apps/client-management/src/app/modules/deduplication/deduplication.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DeduplicationRoutingModule } from './deduplication-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, DeduplicationRoutingModule],
+  imports: [CommonModule, DeduplicationRoutingModule, SharedModule],
 })
 export class DeduplicationModule {}
