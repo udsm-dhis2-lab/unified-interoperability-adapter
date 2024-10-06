ad175051f34426cb8a8401799b045b7c5d9f15db
yuzalsif <liteit68@gmail.com>
2024-10-06 19:40:29 +0300

    chore: Add client details boiler plate code

---
+++ iadapter-applications/apps/client-management/src/app/modules/home/containers/client-details/client-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-client-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './client-details.component.html',
+  styleUrl: './client-details.component.css',
+})
+export class ClientDetailsComponent {}
