735c20b0c676dbb2f7d236d4698c501a16debeaa
yuzalsif <liteit68@gmail.com>
2024-10-06 20:44:05 +0300

    feat: Add support to g back to cient list

--- iadapter-applications/apps/client-management/src/app/modules/home/containers/client-details/client-details.component.ts
+++ iadapter-applications/apps/client-management/src/app/modules/home/containers/client-details/client-details.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'apps/client-management/src/app/shared/shared.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-client-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './client-details.component.html',
   styleUrl: './client-details.component.css',
 })
-export class ClientDetailsComponent {}
+export class ClientDetailsComponent {
+  constructor(private router: Router) {}
+  backToList() {
+    this.router.navigate(['']);
+  }
+}
