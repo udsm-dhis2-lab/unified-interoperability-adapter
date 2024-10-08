07dd8e004a00f7b22b45ec7465502c73536b8dd9
Walter V. Faustine <waltervfaustine@gmail.com>
2024-10-08 10:52:42 +0300

    fix: 🐛 update app component
    This commit will update app component

--- iadapter-applications/apps/workflows-management/src/app/app.routes.ts
+++ iadapter-applications/apps/workflows-management/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        loadChildren: () =>
+            import('../app/pages/home/home.module').then((m) => m.HomeModule),
+    },
+    {
+        path: 'home',
+        loadChildren: () =>
+            import('../app/pages/home/home.module').then((m) => m.HomeModule),
+    },
+    // {
+    //   path: 'deduplication',
+    //   loadChildren: () =>
+    //     import('./modules/deduplication/deduplication.module').then(
+    //       (m) => m.DeduplicationModule
+    //     ),
+    // },
+];
