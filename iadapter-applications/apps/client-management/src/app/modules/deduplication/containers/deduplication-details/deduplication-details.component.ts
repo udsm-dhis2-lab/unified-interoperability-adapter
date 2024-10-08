118da771a273ea4ef9d0d1af651067a69ad193e4
yuzalsif <liteit68@gmail.com>
2024-10-08 06:08:34 +0300

    feat: Add support to select items in deduplicates

--- iadapter-applications/apps/client-management/src/app/modules/deduplication/containers/deduplication-details/deduplication-details.component.ts
+++ iadapter-applications/apps/client-management/src/app/modules/deduplication/containers/deduplication-details/deduplication-details.component.ts
@@ -8,9 +8,11 @@ interface BasicInfo {
 }
 
 interface Deduplication {
+  id: number;
   clientID: string;
   iDNumber: string;
   fullName: string;
+  healthRecords: number;
 }
 
 interface ExtraInfoSection {
@@ -37,19 +39,25 @@ export class DeduplicationDetailsComponent {
 
   listOfData: Deduplication[] = [
     {
+      id: 1,
       clientID: 'IBS_00297209',
       iDNumber: '97782H',
       fullName: 'Herman Moshi Moshi',
+      healthRecords: 3,
     },
     {
+      id: 2,
       clientID: 'IBS_00297209',
       iDNumber: '97782H',
       fullName: 'Herman Moshi Moshi',
+      healthRecords: 2,
     },
     {
+      id: 3,
       clientID: 'IBS_00297209',
       iDNumber: '97782H',
       fullName: 'Herman Moshi Moshi',
+      healthRecords: 7,
     },
   ];
 
@@ -84,6 +92,46 @@ export class DeduplicationDetailsComponent {
       },
     },
   ];
+
+  checked = false;
+  indeterminate = false;
+  listOfCurrentPageData: readonly Deduplication[] = [];
+  setOfCheckedId = new Set<number>();
+
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(value: boolean): void {
+    this.listOfCurrentPageData.forEach((item) =>
+      this.updateCheckedSet(item.id, value)
+    );
+    this.refreshCheckedStatus();
+  }
+
+  onCurrentPageDataChange($event: readonly Deduplication[]): void {
+    this.listOfCurrentPageData = $event;
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    this.checked = this.listOfCurrentPageData.every((item) =>
+      this.setOfCheckedId.has(item.id)
+    );
+    this.indeterminate =
+      this.listOfCurrentPageData.some((item) =>
+        this.setOfCheckedId.has(item.id)
+      ) && !this.checked;
+  }
 
   constructor(private router: Router, private modal: NzModalService) {}
   backToList() {
