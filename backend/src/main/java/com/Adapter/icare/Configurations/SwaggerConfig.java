dd3ab5eae3d67bfb6f26e2c65ce1d1d0931a4b14
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-10-11 08:35:04 +0300

    Add sever for API documentation

--- backend/src/main/java/com/Adapter/icare/Configurations/SwaggerConfig.java
+++ backend/src/main/java/com/Adapter/icare/Configurations/SwaggerConfig.java
@@ -1,7 +1,13 @@
 package com.Adapter.icare.Configurations;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springdoc.core.GroupedOpenApi;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -10,7 +16,20 @@ public class SwaggerConfig {
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("public-api")
-                .pathsToMatch("/api/**")  // Configure paths for Swagger to document
+                .pathsToMatch("/api/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // List of servers
+        List<Server> servers = Arrays.asList(
+                new Server().url("https://iadapter.dhis2.udsm.ac.tz").description("Development Server")
+        );
+
+        return new OpenAPI().info(new Info().title("iAdapterAPI")
+                        .description("This is an API documentation for iAdapter")
+                        .version("v1.0.0"))
+                .servers(servers);
     }
 }
