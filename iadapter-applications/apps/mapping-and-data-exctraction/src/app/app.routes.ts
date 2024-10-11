248a7f640fdca96fb162a272a7f4bb5a07ce0d81
yuzalsif <liteit68@gmail.com>
2024-10-11 12:31:43 +0300

    Add home and dataset-mapping to route

--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/app.routes.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/mappings/mappings.module').then(
+        (m) => m.MappingsModule
+      ),
+  },
+];
--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/mappings.module.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/mappings.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MappingsRoutingModule } from './mappings-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MappingsRoutingModule],
 })
-export class MappingsModule { }
+export class MappingsModule {}
