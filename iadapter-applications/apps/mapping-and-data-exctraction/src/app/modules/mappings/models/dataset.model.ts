28b87b0f3849e3eca93a2c5330135f5fefb45ea6
yuzalsif <liteit68@gmail.com>
2024-10-12 12:16:28 +0300

    fix: Use displayName if nt available use name

--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/models/dataset.model.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/models/dataset.model.ts
@@ -5,12 +5,16 @@ export class Dataset {
   periodType!: string;
   expiryDays!: string;
   formType!: string;
+  name!: string;
+  uuid!: string;
 
   static fromJson(json: any): Dataset {
     let dataset = new Dataset();
     dataset.id = json['id'] ?? '';
+    dataset.name = json['name'];
+    dataset.uuid = json['uuid'] ?? '';
     dataset.code = json['code'] ?? '';
-    dataset.displayName = json['displayName'] ?? '';
+    dataset.displayName = json['displayName'];
     dataset.periodType = json['periodType'] ?? '';
     dataset.formType = json['formType'] ?? '';
     dataset.expiryDays = json['expiryDays'] ?? '';
