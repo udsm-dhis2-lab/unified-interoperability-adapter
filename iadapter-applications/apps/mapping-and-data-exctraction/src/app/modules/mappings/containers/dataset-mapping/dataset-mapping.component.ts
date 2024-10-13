166dcd734e9fa92e1c8e2e1f8f8e223053fc606a
yuzalsif <liteit68@gmail.com>
2024-10-13 23:47:29 +0300

    fix: Sanitize html content to include css & security

--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/containers/dataset-mapping/dataset-mapping.component.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/containers/dataset-mapping/dataset-mapping.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { SharedModule } from 'apps/mapping-and-data-exctraction/src/app/shared/shared.module';
 import { DatasetManagementService } from '../../services/dataset-management.service';
 import { ActivatedRoute } from '@angular/router';
 import { Dataset } from '../../models';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dataset-mapping',
@@ -15,10 +16,12 @@ export class DatasetMappingComponent implements OnInit {
   dataSetUuid: string = '';
   isLoading: boolean = true;
   datasetFormContent: string = '';
+  sanitizedContent!: SafeHtml;
 
   constructor(
     private route: ActivatedRoute,
-    private dataSettementService: DatasetManagementService
+    private dataSettementService: DatasetManagementService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
@@ -31,8 +34,9 @@ export class DatasetMappingComponent implements OnInit {
       next: (data: any) => {
         this.isLoading = false;
         const datasetFields = JSON.parse(data.datasetFields);
-        this.datasetFormContent = datasetFields.dataEntryForm.htmlCode;
-        console.log(this.datasetFormContent, 'DATA HTML');
+        this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(
+          datasetFields.dataEntryForm.htmlCode
+        );
       },
       error: (error: any) => {
         this.isLoading = false;
