01044461c7fa1e275f04115ed69443460642bf2b
yuzalsif <liteit68@gmail.com>
2024-10-14 09:30:14 +0300

    feat: Add support to select input id from custom form

--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/containers/dataset-mapping/dataset-mapping.component.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/containers/dataset-mapping/dataset-mapping.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { SharedModule } from 'apps/mapping-and-data-exctraction/src/app/shared/shared.module';
 import { DatasetManagementService } from '../../services/dataset-management.service';
 import { ActivatedRoute } from '@angular/router';
@@ -18,10 +26,15 @@ export class DatasetMappingComponent implements OnInit {
   datasetFormContent: string = '';
   sanitizedContent!: SafeHtml;
 
+  selectedInputId: string = '';
+
   constructor(
     private route: ActivatedRoute,
     private dataSettementService: DatasetManagementService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private renderer: Renderer2,
+    private elRef: ElementRef,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -37,11 +50,32 @@ export class DatasetMappingComponent implements OnInit {
         this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(
           datasetFields.dataEntryForm.htmlCode
         );
+        this.cdr.detectChanges();
+        this.addFocusListeners();
       },
       error: (error: any) => {
         this.isLoading = false;
         // TODO: Implement error handling
       },
     });
+  }
+
+  addFocusListeners(): void {
+    const inputElements = this.elRef.nativeElement.querySelectorAll('input');
+    inputElements.forEach((input: HTMLInputElement) => {
+      this.renderer.listen(input, 'focus', (event) => this.onInputFocus(event));
+    });
+  }
+
+  onInputFocus(event: FocusEvent): void {
+    const inputElement = event.target as HTMLInputElement;
+    const inputId = inputElement.id;
+    this.selectedInputId = inputId;
+    const isDisabled = inputElement.disabled;
+    // if (!isDisabled) {
+    //   this.renderer.setStyle(inputElement, 'background-color', '#2C6693');
+    //   this.renderer.setProperty(inputElement, 'disabled', true);
+
+    // }
   }
 }
