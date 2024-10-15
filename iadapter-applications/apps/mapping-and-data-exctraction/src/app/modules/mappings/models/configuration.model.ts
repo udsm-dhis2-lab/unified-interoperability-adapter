69b8a8203a8b3afd081943a19004095772673583
yuzalsif <liteit68@gmail.com>
2024-10-15 09:41:05 +0300

    Restructure Configuration model to match the new payload

--- iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/models/configuration.model.ts
+++ iadapter-applications/apps/mapping-and-data-exctraction/src/app/modules/mappings/models/configuration.model.ts
@@ -2,51 +2,27 @@ import { Option } from './options.model';
 
 export class Configuration {
   key!: string;
-  group!: string;
-  value!: ConfigurationValue;
-
-  static fromJson(json: any): Configuration {
-    console.log('JSONNN', json);
-    const configuration = new Configuration();
-    configuration.key = json['key'];
-    console.log('KEY', json['key']);
-    configuration.group = json['group'];
-    configuration.value = ConfigurationValue.fromJson(json['value']);
-    return configuration;
-  }
-
-  toJson (): any {
-    return {
-      key: this.key,
-      group: this.group,
-      value: this.value.toJson(),
-    };
-  }
-}
-
-export class ConfigurationValue {
-  code!: string;
   name!: string;
-  key!: string;
+  code!: string;
   options!: Option[];
 
-  static fromJson(json: any): ConfigurationValue {
-    const configurationValue = new ConfigurationValue();
-    configurationValue.code = json['code'];
-    configurationValue.name = json['name'];
-    configurationValue.key = json['key'];
-    configurationValue.options = (json['options'] ?? []).map((option: any) =>
-      Option.fromJson(option)
+  static fromJson(json: any): Configuration {
+    const configuration = new Configuration();
+    configuration.key = json['key'];
+    configuration.name = json['name'];
+    configuration.code = json['code'];
+    configuration.options = json['options'].map((item: any) =>
+      Option.fromJson(item)
     );
-    return configurationValue;
+    return configuration;
   }
 
   toJson(): any {
     return {
-      code: this.code,
-      name: this.name,
       key: this.key,
-      options: this.options.map((option) => option.toJson()),
+      name: this.name,
+      code: this.code,
+      options: [...this.options].map((item) => item.toJson()),
     };
   }
 }
