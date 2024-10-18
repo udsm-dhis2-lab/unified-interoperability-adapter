cbd89089ab1700357f21342f628de108dc58a59d
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-10-18 10:07:40 +0300

    Change location of application.properties file

--- backend/src/main/java/com/Adapter/icare/IcareApplication.java
+++ backend/src/main/java/com/Adapter/icare/IcareApplication.java
@@ -36,12 +36,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import java.util.Collections;
+
 @SpringBootApplication(scanBasePackages = "com.Adapter.icare")
 public class IcareApplication extends SpringBootServletInitializer
 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IcareApplication.class, args);
+		SpringApplication app = new SpringApplication(IcareApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.config.location", "file:src/config/"));
+		app.run(args);
 	}
 
 }
