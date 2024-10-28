9bc33e903d2834217b6910deceeab8d3ad51d9ed
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-10-28 15:00:01 +0300

    Change referral details DTO

--- backend/src/main/java/com/Adapter/icare/Dtos/ReferralDetailsDTO.java
+++ backend/src/main/java/com/Adapter/icare/Dtos/ReferralDetailsDTO.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
 public class ReferralDetailsDTO {
     private Date referralDate;
-    private String reason;
+    private List<String> reasons;
+    private String hfrCode;
+    private String referralNo;
     private Map<String,Object> referringClinician; // TODO: Add specific model
 }
