ddde09dcfe9785a290f8d8356936054c285f026d
josephatJ <josephatjulius24@gmail.com>
2024-11-04 18:28:44 +0300

    Add not null on referralDate, referralNumber and hfrCode

--- backend/src/main/java/com/Adapter/icare/Dtos/ReferralDetailsDTO.java
+++ backend/src/main/java/com/Adapter/icare/Dtos/ReferralDetailsDTO.java
@@ -3,6 +3,7 @@ package com.Adapter.icare.Dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +11,14 @@ import java.util.Map;
 @Getter
 @Setter
 public class ReferralDetailsDTO {
+    @NotNull
     private Date referralDate;
     private List<String> reason;
+
+    @NotNull
     private String referralNumber;
+
+    @NotNull
     private String hfrCode;
     private Map<String,Object> referringClinician; // TODO: Add specific model
 }
