e6294012dece2498aa82bc594e66eba3b5926c9f
josephatJ <josephatjulius24@gmail.com>
2024-11-12 12:34:07 +0300

    Add support to extract investigation details from observations

--- backend/src/main/java/com/Adapter/icare/SharedHealthRecords/Services/SharedHealthRecordsService.java
+++ backend/src/main/java/com/Adapter/icare/SharedHealthRecords/Services/SharedHealthRecordsService.java
@@ -510,6 +510,85 @@ public class SharedHealthRecordsService {
                             templateData.setDiagnosisDetails(diagnosisDetailsDTOS);
 
                             templateData.setAllergies(allergiesDTOS);
+
+                            // Investigation details
+//                            caseClassification
+//                                    dateOccurred
+//                            daysSinceSymptoms
+//                                    diseaseCode
+//                            labSpecimenTaken
+//                                    specimenSentTo
+//                            vaccinated
+                            List<InvestigationDetailsDTO> investigationDetailsDTOList = new ArrayList<>();
+                            List<Observation> investigationDetailsGroup =  getObservationsByCategory("investigation-details", encounter, true);
+                            // Visit notes
+                            if (!investigationDetailsGroup.isEmpty()) {
+                                for (Observation observationGroup : investigationDetailsGroup) {
+                                    InvestigationDetailsDTO investigationDetailsDTO = new InvestigationDetailsDTO();
+                                    List<Observation> caseClassificationData = getObservationsByObservationGroupId("case-classification", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!caseClassificationData.isEmpty()) {
+                                        for (Observation observation : caseClassificationData) {
+                                            if (observation.hasValueStringType()) {
+                                                investigationDetailsDTO.setCaseClassification(observation.getValueStringType().toString());
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    investigationDetailsDTO.setDateOccurred(observationGroup.hasEffectiveDateTimeType() ? observationGroup.getEffectiveDateTimeType().getValue(): null);
+
+                                    List<Observation> daysSinceSymptomsData = getObservationsByObservationGroupId("days-since-symptoms", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!daysSinceSymptomsData.isEmpty()) {
+                                        for (Observation observation : daysSinceSymptomsData) {
+                                            if (observation.hasValueQuantity()) {
+                                                investigationDetailsDTO.setDaysSinceSymptoms(observation.getValueQuantity().getValue().intValueExact());
+                                                break;
+                                            }
+                                        }
+                                    }
+
+                                    List<Observation> diseaseCodeData = getObservationsByObservationGroupId("disease-code", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!diseaseCodeData.isEmpty()) {
+                                        for (Observation observation : diseaseCodeData) {
+                                            if (observation.hasValueCodeableConcept()) {
+                                                investigationDetailsDTO.setDiseaseCode(observation.getValueCodeableConcept().getCoding().get(0).getCode());
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    List<Observation> labSpecimenTakenData = getObservationsByObservationGroupId("lab-specimen-taken", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!labSpecimenTakenData.isEmpty()) {
+                                        for (Observation observation : labSpecimenTakenData) {
+                                            if (observation.hasValueStringType()) {
+                                                investigationDetailsDTO.setLabSpecimenTaken(observation.getValueStringType().toString());
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    List<Observation> specimenSentToData = getObservationsByObservationGroupId("specimen-sent-to", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!specimenSentToData.isEmpty()) {
+                                        for (Observation observation : specimenSentToData) {
+                                            if (observation.hasValueStringType()) {
+                                                investigationDetailsDTO.setSpecimenSentTo(observation.getValueStringType().toString());
+                                                break;
+                                            }
+                                        }
+                                    }
+
+                                    List<Observation> vaccinatedData = getObservationsByObservationGroupId("vaccinated", encounter, observationGroup.getIdElement().getIdPart());
+                                    if (!vaccinatedData.isEmpty()) {
+                                        for (Observation observation : vaccinatedData) {
+                                            if (observation.hasValueStringType()) {
+                                                investigationDetailsDTO.setVaccinated(observation.getValueStringType().toString());
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    investigationDetailsDTOList.add(investigationDetailsDTO);
+                                }
+                            }
+
+                            templateData.setInvestigationDetails(investigationDetailsDTOList);
+
                             ReferralDetailsDTO referralDetailsDTO = new ReferralDetailsDTO();
                             List<Identifier> identifiers = encounter.getIdentifier();
                             for (Identifier identifierData: identifiers) {
