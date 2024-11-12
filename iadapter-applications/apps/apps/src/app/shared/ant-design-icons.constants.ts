5439c8d86b2ac8768770047945f25a68a9ba2cfb
yuzalsif <liteit68@gmail.com>
2024-11-12 11:36:53 +0300

    chore: Add logout icoon in "apps" application

--- iadapter-applications/apps/apps/src/app/shared/ant-design-icons.constants.ts
+++ iadapter-applications/apps/apps/src/app/shared/ant-design-icons.constants.ts
@@ -1,15 +1,17 @@
 import {
-    ApartmentOutline,
-    DashboardOutline,
-    MergeOutline,
-    UnorderedListOutline,
-    UserOutline,
-  } from '@ant-design/icons-angular/icons';
-  
-  export const antDesignIcons: any[] = [
-    UserOutline,
-    UnorderedListOutline,
-    MergeOutline,
-    ApartmentOutline,
-    DashboardOutline,
-  ];
+  ApartmentOutline,
+  DashboardOutline,
+  MergeOutline,
+  UnorderedListOutline,
+  UserOutline,
+  LogoutOutline,
+} from '@ant-design/icons-angular/icons';
+
+export const antDesignIcons: any[] = [
+  UserOutline,
+  UnorderedListOutline,
+  MergeOutline,
+  ApartmentOutline,
+  DashboardOutline,
+  LogoutOutline,
+];
