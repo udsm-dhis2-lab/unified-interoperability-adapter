ebdc8e72975fde58af48f2614ab88efc4ad72763
josephatJ <josephatjulius24@gmail.com>
2024-11-22 18:10:16 +0300

    Add sample aggregate query creation script to be used on workflow engine

---
+++ scripts/javascript/aggregation-queries/create-query.js
@@ -0,0 +1,586 @@
+// Sample mappings array
+const mappings = [
+  {
+    uuid: "5cd8d3c0-25f5-44ce-ae33-389b2e01bb69",
+    dataKey: "Cholera",
+    mapping: {
+      mappings: [
+        {
+          code: "A00.1",
+        },
+        {
+          code: "A00.2",
+        },
+        {
+          code: "A00",
+        },
+        {
+          code: "A95",
+        },
+      ],
+      dataElement: {
+        id: "CsYfybuaSgc",
+        name: "Cholera",
+        code: "",
+      },
+      type: "diagnosisDetails",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+  {
+    uuid: "88615825-f47a-478c-9eba-dee68129b27f",
+    dataKey: "New-visits",
+    mapping: {
+      dataElement: {
+        id: "Yut5amdi7iw",
+        name: "Mahudhurio ya kwanza/wagonjwa wapya (kwenye kituo husika kwa tatizo fulani la kiafya)",
+        code: "",
+      },
+      type: "visitDetails.new",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+  {
+    uuid: "d9a84c5e-36f6-4938-bf90-80b44e47c204",
+    dataKey: "New-visits-this-year",
+    mapping: {
+      dataElement: {
+        id: "uyQpafHrxLT",
+        name: "Mara ya Kwanza Kuhudhuria OPD Mwaka Huu",
+        code: "",
+      },
+      type: "visitDetails.newThisYear",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+  {
+    uuid: "a24d56cf-d0e1-4583-b6d4-cb7cbba4bafc",
+    dataKey: "Referrals",
+    mapping: {
+      dataElement: {
+        id: "wwl4tRTnPEo",
+        name: "Referred Out from OPD",
+        code: "",
+      },
+      type: "outcomeDetails.referred",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+  {
+    uuid: "9060363f-16db-4279-84ff-5634226d6d01",
+    dataKey: "Repeat-visits",
+    mapping: {
+      dataElement: {
+        id: "KlePTLpBdWd",
+        name: "Mahudhurio ya Marudio Katika OPD",
+        code: "",
+      },
+      type: "visitDetails.repeat",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+  {
+    uuid: "13a85bf5-e992-4be0-ac71-34877cdf86eb",
+    dataKey: "Typhoid",
+    mapping: {
+      mappings: [
+        {
+          code: "A01.1",
+        },
+        {
+          code: "A01.2",
+        },
+        {
+          code: "A01",
+        },
+      ],
+      dataElement: {
+        id: "mcVhgPQtLLX",
+        name: "Typhoid",
+        code: "",
+      },
+      type: "diagnosisDetails",
+      params: [
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "M",
+          co: "xo7RVsgBkpT",
+          ADXKeys: ["GENDER='M'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "M",
+          co: "n15V8E7ZLT9",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "M",
+          co: "PSQ2DsGun8c",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "M",
+          co: "ddmrU8qJa7L",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "M",
+          co: "aYgz5mh2cbx",
+        },
+        {
+          ageType: "days",
+          startAge: 0,
+          endAge: 32,
+          gender: "F",
+          co: "Ak7SvGtlnWp",
+          ADXKeys: ["GENDER='F'", "AGE='CHINI-YA-MWEZI-1'"],
+        },
+        {
+          ageType: "months",
+          startAge: 1,
+          endAge: 11,
+          gender: "F",
+          co: "R9Ka4Hd70Wh",
+        },
+        {
+          ageType: "years",
+          startAge: 1,
+          endAge: 5,
+          gender: "F",
+          co: "kSxbMW8Lmsd",
+        },
+        {
+          ageType: "years",
+          startAge: 5,
+          endAge: 60,
+          gender: "F",
+          co: "sgG28zSclZm",
+        },
+        {
+          ageType: "years",
+          startAge: 60,
+          endAge: 1000,
+          gender: "F",
+          co: "ayVLc9to6xY",
+        },
+      ],
+    },
+    namespace: "MAPPINGS-HMIS-OPD",
+    description: null,
+    group: null,
+  },
+];
+
+mappings.forEach((mapping) => {
+  const dataElementId = mapping.mapping.dataElement.id;
+  let query = `SELECT\n`;
+
+  query += `  '${dataElementId}' AS "${dataElementId}",\n`;
+
+  mapping.mapping.params.forEach((param, index) => {
+    const gender = param.gender;
+    const co = param.co;
+    const ageType = param.ageType;
+    const startAge = param.startAge;
+    const endAge = param.endAge;
+
+    query += `  COUNT(*) FILTER (WHERE gender = '${
+      gender === "M" ? "male" : "female"
+    }') AS "${co}"${index < mapping.mapping.params.length - 1 ? "," : ""} \n`;
+  });
+
+  query += ` FROM patient_flat `;
+  query += ` GROUP BY gender`;
+
+  console.log(query);
+});
