134409f1bafddea86a97be1aef43703b5d955733
josephatJ <josephatjulius24@gmail.com>
2024-11-28 08:41:28 +0300

    Add toMap functions for data template data dto

--- backend/src/main/java/com/Adapter/icare/Dtos/DataTemplateDataDTO.java
+++ backend/src/main/java/com/Adapter/icare/Dtos/DataTemplateDataDTO.java
@@ -3,6 +3,7 @@ package com.Adapter.icare.Dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,4 +14,13 @@ public class DataTemplateDataDTO {
     private List<SharedHealthRecordsDTO> listGrid;
     private ReportDetailsDTO reportDetails;
     private List<IdentifierDTO> clientIdentifiersPool;
+
+    public Map<String,Object> toMap() {
+        Map<String, Object> mappedDataTemplateData = new HashMap<>();
+        mappedDataTemplateData.put("facilityDetails", this.getFacilityDetails().toMap());
+        mappedDataTemplateData.put("listGrid", this.getListGrid().stream().map(sharedHealthRecordsDTO -> sharedHealthRecordsDTO.toMap()));
+        mappedDataTemplateData.put("reportDetails", this.getReportDetails());
+        mappedDataTemplateData.put("clientIdentifiersPool", this.getClientIdentifiersPool());
+        return mappedDataTemplateData;
+    }
 }
