8136354df9f2a5b701635b1040d1b7a0941991b4
yuzalsif <liteit68@gmail.com>
2024-12-08 19:36:42 +0300

    feat: Add script to migrate configurations

---
+++ scripts/python/configuration-migration.py
@@ -0,0 +1,58 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import asyncio
+import os
+from dotenv import load_dotenv
+import json
+import ast
+
+load_dotenv()
+
+username =  os.getenv('HDU_USERNAME')
+password = os.getenv('HDU_PASSWORD')
+
+headers = {
+    'Content-Type': 'application/json; charset=utf8'
+}
+
+source_url ="https://iadapter.dhis2.udsm.ac.tz/api/v1/hduApi/configurations"
+
+async def get_conigurations():
+    path =source_url + "?page=1&pageSize=100"
+    print(path)
+    response = requests.get(path, auth=(username,password), verify=False)
+    if response.status_code == 200 or response.status_code == 201:
+        payload = json.loads(response.content)
+        return payload
+    else:
+        return "none"
+    
+async def save_conifguration(payload):
+    payload['value'] = {
+    'key': payload.get('key'),
+    'code': payload.get('key'),
+    'name': payload.get('name'),
+    'keyToUseInMappings': payload.get('keyToUseInMappings'),
+    'options': payload.get('options'),
+}
+    payload.pop('uuid', None)
+    payload.pop('code', None)
+    payload.pop('name', None)
+    payload.pop('keyToUseInMappings', None)
+    payload.pop('options', None)
+    print(payload)
+    response = requests.post("http://41.59.228.177/api/v1/hduApi/configurations", 
+                                    json=payload, auth=(username,password), 
+                                    headers=headers)
+    return response
+
+async def main():
+    configurations = await get_conigurations()
+    if configurations != "none":
+        print(len(configurations["results"]))
+        for configuration in configurations["results"]:
+            response = await save_conifguration(configuration)
+            print(response)
+    
+
+asyncio.run(main())
