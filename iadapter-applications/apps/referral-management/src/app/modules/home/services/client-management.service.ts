79d210dc85b3b59520c46d08aeaa18194cf85f6a
wile44 <goodluckwileonline@gmail.com>
2025-03-14 16:07:09 +0300

    feat: improve client management service

--- iadapter-applications/apps/referral-management/src/app/modules/home/services/client-management.service.ts
+++ iadapter-applications/apps/referral-management/src/app/modules/home/services/client-management.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 import { HduHttpService } from 'libs/hdu-api-http-client/src/lib/services/hdu-http.service';
 import { ClientUrls } from '../models';
 import { catchError, map, Observable } from 'rxjs';
-import { HttpParams } from '@angular/common/http';
+import { HttpHeaders, HttpParams, HttpClient } from '@angular/common/http';
 import {
   UnAuothorizedException,
   UnknownException,
-} from '../../../../../../../libs/models';
+} from '../../../../../../../libs/models/src';
 import { ClientPage } from '../models';
 
 @Injectable()
@@ -15,7 +15,14 @@ export class ClientManagementService {
   hduClientsUrl: string = ClientUrls.GET_CLIENTS;
   hduSharedRecordsUrl: string = ClientUrls.GET_SHARED_RECORDS;
 
-  constructor(private httpClient: HduHttpService) {}
+  // TODO: softcode authentification credentials
+  private readonly apiUrl = 'http://41.59.228.177/engine/processes/FHIR-REFERRAL-QUERY/run?async=true';
+  private readonly credentials = {
+    username: 'admin',
+    password: 'Admin123'
+  };
+
+  constructor(private httpClient: HduHttpService,private http: HttpClient) {}
 
   getHduClients(
     pageIndex: number,
@@ -83,4 +90,56 @@ export class ClientManagementService {
         })
       );
   }
+
+  getClientById(
+    id: string
+  ): Observable<ClientPage> {
+
+    return this.httpClient
+      .get<{ results: any }>(`${this.hduSharedRecordsUrl}?id=${id}`)
+      .pipe(
+        map((response: { results: any }) => {
+          return ClientPage.fromJson(response);
+        }),
+        catchError((error: any) => {
+          if (error.status === 401) {
+            throw new UnAuothorizedException('Invalid username or password');
+          }
+          throw new UnknownException(
+            'An unexpected error occurred. Please try again later.'
+          );
+        })
+      );
+  }
+
+  private getHeaders(): HttpHeaders {
+    const authHeader = 'Basic ' + btoa(`${this.credentials.username}:${this.credentials.password}`);
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': authHeader
+    });
+  }
+
+  getReferrals(filters: any): Observable<Referral[]> {
+    return this.http.post<Referral[]>(this.apiUrl,
+      filters
+      ,
+      {
+      headers: this.getHeaders()
+
+    });
+  }
+}
+
+
+export interface Referral {
+  client_id: string;
+  first_name: string;
+  surname: string;
+  gender: string;
+  referral_date: string;
+  referral_number: string;
+  referred_to_other_country: boolean | null;
+  referring_facility_code: string | null;
+  referring_facility: string | null;
 }
