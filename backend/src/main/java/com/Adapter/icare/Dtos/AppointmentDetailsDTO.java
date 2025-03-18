0fc9c73733473f53558df2e500c24ab19eba2952
yuzalsif <yuseifabdillah@gmail.com>
2025-03-18 11:02:57 +0300

    feat: Add appointment details

--- backend/src/main/java/com/Adapter/icare/Dtos/AppointmentDetailsDTO.java
+++ backend/src/main/java/com/Adapter/icare/Dtos/AppointmentDetailsDTO.java
@@ -1,4 +1,30 @@
 package com.Adapter.icare.Dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
 public class AppointmentDetailsDTO {
+    private String appointmentId;
+    private String hfrCode;
+    private String appointmentStatus;
+    private List<AppointmentPaymentDetailsDTO> paymentDetails;
+    private List<AppointmentServiceDetailsDTO> serviceDetails;
+
+    Map<String, Object> toMap() {
+        Map<String, Object> appointmentDetails = new HashMap<>();
+
+        appointmentDetails.put("appointmentId", this.getAppointmentId());
+        appointmentDetails.put("hfrCode", this.getHfrCode());
+        appointmentDetails.put("appointmentStatus", this.getAppointmentStatus());
+        appointmentDetails.put("paymentDetails", this.getPaymentDetails().stream().map(AppointmentPaymentDetailsDTO::toMap));
+        appointmentDetails.put("serviceDetails", this.getServiceDetails().stream().map(AppointmentServiceDetailsDTO::toMap));
+
+        return appointmentDetails;
+    }
 }
