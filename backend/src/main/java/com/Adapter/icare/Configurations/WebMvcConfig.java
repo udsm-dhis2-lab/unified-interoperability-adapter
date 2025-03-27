a5b9e1668cf64b01473059f90728da607b7121f8
Bennett Benedict <bennybenester@gmail.com>
2025-03-27 09:09:37 +0300

    release: Improve builds and local development

--- backend/src/main/java/com/Adapter/icare/Configurations/WebMvcConfig.java
+++ backend/src/main/java/com/Adapter/icare/Configurations/WebMvcConfig.java
@@ -2,23 +2,22 @@ package com.Adapter.icare.Configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if (!registry.hasMappingForPattern("/assets/**")) {
             registry.addResourceHandler("/assets/**")
                     .addResourceLocations("/assets");
         }
 
-//        registry.addResourceHandler("/login/**")
-//                .addResourceLocations("classpath:/static/login/");
-//
-//        registry.addResourceHandler("/ClientManagementService/**")
-//                .addResourceLocations("classpath:/static/ClientManagementService/");
+        // registry.addResourceHandler("/login/**")
+        // .addResourceLocations("classpath:/static/login/");
+        //
+        // registry.addResourceHandler("/ClientManagementService/**")
+        // .addResourceLocations("classpath:/static/ClientManagementService/");
     }
 }
