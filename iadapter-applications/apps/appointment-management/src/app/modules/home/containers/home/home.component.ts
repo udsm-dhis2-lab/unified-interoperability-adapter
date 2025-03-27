f0e5442a67e773904aa9844071b0b65b9ae544de
Bennett Benedict <bennybenester@gmail.com>
2025-03-27 12:49:41 +0300

    release: Add functionality to build all apps

--- iadapter-applications/apps/appointment-management/src/app/modules/home/containers/home/home.component.ts
+++ iadapter-applications/apps/appointment-management/src/app/modules/home/containers/home/home.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { SharedModule } from 'apps/client-management/src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { HDUAPIClientDetails } from '../../models';
-import { ClientManagementService } from '../../services/client-management.service';
-import { NzTableQueryParams } from 'ng-zorro-antd/table';
-import { Subscription } from 'rxjs';
-import { SearchBarComponent } from '../../../../../../../../libs/search-bar/src/lib/search-bar/search-bar.component';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzInputModule } from 'ng-zorro-antd/input';
+import { SharedModule } from 'apps/client-management/src/app/shared/shared.module';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
 import { NzTagModule } from 'ng-zorro-antd/tag';
+import { Subscription } from 'rxjs';
+import { SearchBarComponent } from '../../../../../../../../libs/search-bar/src/lib/search-bar/search-bar.component';
+import { ClientManagementService } from '../../services/client-management.service';
 
 @Component({
   selector: 'app-home',
