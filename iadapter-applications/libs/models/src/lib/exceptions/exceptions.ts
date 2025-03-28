fca8df017e3f1841d7e70e4a815bf02607f852f2
Bennett Benedict <bennybenester@gmail.com>
2025-03-28 11:04:50 +0300

    Add interceptor for forbidden calls

--- iadapter-applications/libs/models/src/lib/exceptions/exceptions.ts
+++ iadapter-applications/libs/models/src/lib/exceptions/exceptions.ts
@@ -15,3 +15,36 @@ export class InternalServerException extends Error {
     super(message);
   }
 }
+
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.router.navigate(['/login'], {
+            queryParams: { sessionExpired: true },
+          });
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
