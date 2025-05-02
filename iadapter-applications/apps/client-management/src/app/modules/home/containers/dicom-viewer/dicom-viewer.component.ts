59ef724b52f4a25c94c84dd6c3f8d0a779112312
Bennett Benedict <bennybenester@gmail.com>
2025-05-02 16:10:15 +0300

    release: Add functionality to show json on error message

--- iadapter-applications/apps/client-management/src/app/modules/home/containers/dicom-viewer/dicom-viewer.component.ts
+++ iadapter-applications/apps/client-management/src/app/modules/home/containers/dicom-viewer/dicom-viewer.component.ts
@@ -68,7 +68,6 @@ export class DicomViewerComponent implements OnInit, OnDestroy {
 
     this.dwvApp.addEventListener('error', (event: { error: string }) => {
       this.loading = false;
-      console.log(event);
       this.errorMessage = event['error'];
       if (typeof this.errorMessage !== 'string') {
         this.errorMessage = JSON.stringify(this.errorMessage);
