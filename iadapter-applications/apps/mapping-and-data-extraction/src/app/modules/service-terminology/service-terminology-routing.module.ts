94ea7562420c3986c636a058a4ae7238321f06c8
yuzalsif <yuseifabdillah@gmail.com>
2025-05-23 12:05:56 +0300

    Add routing module

---
+++ iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/service-terminology-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
+import { GeneralCodesComponent } from './containers/general-codes/general-codes.component';
+import { StandardCodesComponent } from './containers/standard-codes/standard-codes.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'general-codes',
+        component: GeneralCodesComponent,
+    },
+    {
+        path: 'standard-codes',
+        component: StandardCodesComponent,
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class MappingsRoutingModule { }
