10750c41c259a4b501336bef8a36234754a6e19e
yuzalsif <yuseifabdillah@gmail.com>
2025-05-26 11:54:02 +0300

    feat: Add function to submit general codes

--- iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/containers/general-codes/general-codes.component.ts
+++ iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/containers/general-codes/general-codes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'apps/mapping-and-data-extraction/src/app/shared/shared.module';
+import { group } from 'console';
+import { ServiceTerminologyConstants } from '../../models/constants/service-terminology-constants';
 
 
 @Component({
@@ -29,6 +31,7 @@ export class GeneralCodesComponent implements OnInit {
       generalCodeType: ['', [Validators.required]],
       code: ['', [Validators.required, Validators.maxLength(50)]],
       name: ['', [Validators.required, Validators.maxLength(100)]],
+      key: ['', [Validators.required, Validators.maxLength(50)]],
     });
   }
 
@@ -42,9 +45,24 @@ export class GeneralCodesComponent implements OnInit {
     return this.generalCodeForm.get('name');
   }
 
+  get key() {
+    return this.generalCodeForm.get('key');
+  }
+
   submitForm(): void {
     if (this.generalCodeForm.valid) {
-      console.log('Form Submitted!', this.generalCodeForm.value);
+      console.log('Form is valid');
+      var generalCode = {
+        namespace: this.generalCodeForm.get('generalCodeType')?.value,
+        value: {
+          code: this.generalCodeForm.get('code')?.value,
+          name: this.generalCodeForm.get('name')?.value
+        },
+        dataKey: this.generalCodeForm.get('key')?.value,
+        datastoreGroup: ServiceTerminologyConstants.GENERAL_CODE_GROUP
+      };
+
+      console.log(this.generalCodeForm.value);
     } else {
       Object.values(this.generalCodeForm.controls).forEach(control => {
         if (control.invalid) {
--- iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/services/services-terminology-service.service.ts
+++ iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/services/services-terminology-service.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HduHttpService } from '@iadapter-applications/hdu-api-http-client';
+import { ServiceTerminologyConstants } from '../models/constants/service-terminology-constants';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesTerminologyServiceService {
+  dataStoreUrl: string = ServiceTerminologyConstants.DATA_STORE;
 
-  constructor() { }
+  constructor(private httpClient: HduHttpService) { }
+
+  saveGeneralCodes(data: any): Observable<any> {
+    return this.httpClient.post(this.dataStoreUrl, data).pipe(
+      map((response: any) => response),
+      catchError((error: any) => {
+        console.error('Error saving general codes:', error);
+        return throwError(error);
+      })
+    );
+  }
 }
