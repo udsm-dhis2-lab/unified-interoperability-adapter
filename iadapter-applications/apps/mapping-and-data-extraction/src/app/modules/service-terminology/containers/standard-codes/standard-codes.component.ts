86e896d4769701262f911936a69ecfc23519852e
yuzalsif <yuseifabdillah@gmail.com>
2025-05-28 10:03:15 +0300

    feat: Add standard code form

--- iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/containers/standard-codes/standard-codes.component.ts
+++ iadapter-applications/apps/mapping-and-data-extraction/src/app/modules/service-terminology/containers/standard-codes/standard-codes.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from 'apps/mapping-and-data-extraction/src/app/shared/shared.module';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms'; 
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-standard-codes',
   templateUrl: './standard-codes.component.html',
-  styleUrl: './standard-codes.component.css',
+  styleUrls: ['./standard-codes.component.css'],
   standalone: true,
-  imports: [SharedModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule, 
+    NzFormModule,
+    NzInputModule,
+    NzSelectModule,
+    NzButtonModule
+  ],
 })
-export class StandardCodesComponent { }
+export class StandardCodesComponent implements OnInit {
+  standardCodeForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.standardCodeForm = this.fb.group({
+      codeType: [null, [Validators.required]],
+      code: [null, [Validators.required]],
+      displayName: [null, [Validators.required]]
+    });
+  }
+
+  submitForm(): void {
+    if (this.standardCodeForm.valid) {
+      console.log('submit', this.standardCodeForm.value);
+    } else {
+      Object.values(this.standardCodeForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+}
