fdeb35106c83cfc043305dd469686274af406e7a
Jonas1015 <jonasgsmart1015@gmail.com>
2025-08-01 12:23:14 +0300

    release: fix response for shared health records

--- backend/src/main/java/com/Adapter/icare/Dtos/BillingsDetailsDTO.java
+++ backend/src/main/java/com/Adapter/icare/Dtos/BillingsDetailsDTO.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -22,4 +24,18 @@ public class BillingsDetailsDTO {
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private String billDate;
+
+    public Map<String, Object> toMap(){
+        Map<String, Object> billingMap = new HashMap<>();
+        billingMap.put("billingID", this.getBillID());
+        billingMap.put("billingCode", this.getBillingCode());
+        billingMap.put("billType", this.getBillType());
+        billingMap.put("insuranceCode", this.getInsuranceCode());
+        billingMap.put("insuranceName", this.getInsuranceName());
+        billingMap.put("amountBilled", this.getAmountBilled());
+        billingMap.put("exemptionType", this.getExemptionType());
+        billingMap.put("wavedAmount", this.getWavedAmount());
+        billingMap.put("standardCode", this.getStandardCode());
+        return billingMap;
+    }
 }
