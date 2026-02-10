6e8a22fc6f781b4c1dadfbd4d9d1ab5813fad395
Jonas1015 <jonasgsmart1015@gmail.com>
2026-02-10 14:54:40 +0300

    Fix /me route to get logged in user

--- backend/src/main/java/com/Adapter/icare/Repository/UserRepository.java
+++ backend/src/main/java/com/Adapter/icare/Repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.Adapter.icare.Domains.User;
 import java.util.List;
@@ -21,8 +22,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM users u", nativeQuery = true)
     List<User> findAllActiveUsers();
     
-    @Query(value = "SELECT * FROM users u", 
-           countQuery = "SELECT count(*) FROM users u",
+    @Query(value = "SELECT * FROM users u " +
+            "WHERE (:search IS NULL OR first_name LIKE CONCAT('%', :search, '%'))" +
+            "AND (:search IS NULL OR middle_name LIKE CONCAT('%', :search, '%'))" +
+            "AND (:search IS NULL OR surname LIKE CONCAT('%', :search, '%'))",
+           countQuery =  "SELECT COUNT(*) FROM users u " +
+                   "WHERE (:search IS NULL OR first_name LIKE CONCAT('%', :search, '%'))" +
+                   "AND (:search IS NULL OR middle_name LIKE CONCAT('%', :search, '%'))" +
+                   "AND (:search IS NULL OR surname LIKE CONCAT('%', :search, '%'))",
            nativeQuery = true)
-    Page<User> findAllActiveUsers(Pageable pageable);
+    Page<User> findAllActiveUsers(Pageable pageable, @Param("search") String search);
 }
