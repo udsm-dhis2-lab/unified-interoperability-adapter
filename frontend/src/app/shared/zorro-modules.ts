6d637162d01058747928e9ade9381ee549e927ad
yuzalsif <yuseifabdillah@gmail.com>
2026-03-09 12:13:28 +0300

    Add NzSelectModule to zorro modules

--- frontend/src/app/shared/zorro-modules.ts
+++ frontend/src/app/shared/zorro-modules.ts
@@ -1,38 +1,38 @@
-import { NzAlertModule } from "ng-zorro-antd/alert";
-import { NzAvatarModule } from "ng-zorro-antd/avatar";
-import { NzBadgeModule } from "ng-zorro-antd/badge";
-import { NzButtonModule } from "ng-zorro-antd/button";
-import { NzCardModule } from "ng-zorro-antd/card";
-import { NzCheckboxModule } from "ng-zorro-antd/checkbox";
-import { NzCollapseModule } from "ng-zorro-antd/collapse";
-import { NzDatePickerModule } from "ng-zorro-antd/date-picker";
-import { NzDescriptionsModule } from "ng-zorro-antd/descriptions";
-import { NzDividerModule } from "ng-zorro-antd/divider";
-import { NzDrawerModule } from "ng-zorro-antd/drawer";
-import { NzDropdownModule } from "ng-zorro-antd/dropdown";
-import { NzFormModule } from "ng-zorro-antd/form";
-import { NzGridModule } from "ng-zorro-antd/grid";
-import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzInputModule } from "ng-zorro-antd/input";
-import { NzListModule } from "ng-zorro-antd/list";
-import { NzModalModule } from "ng-zorro-antd/modal";
-import { NzPaginationModule } from "ng-zorro-antd/pagination";
-import { NzProgressModule } from "ng-zorro-antd/progress";
-import { NzRadioModule } from "ng-zorro-antd/radio";
-import { NzSelectModule } from "ng-zorro-antd/select";
-import { NzSpaceModule } from "ng-zorro-antd/space";
-import { NzSpinComponent } from "ng-zorro-antd/spin";
-import { NzStatisticModule } from "ng-zorro-antd/statistic";
-import { NzStepsModule } from "ng-zorro-antd/steps";
-import { NzSwitchModule } from "ng-zorro-antd/switch";
-import { NzTableModule } from "ng-zorro-antd/table";
-import { NzTabsModule } from "ng-zorro-antd/tabs";
-import { NzTagModule } from "ng-zorro-antd/tag";
-import { NzTimelineModule } from "ng-zorro-antd/timeline";
-import { NzTooltipModule } from "ng-zorro-antd/tooltip";
-import { NzTypographyModule } from "ng-zorro-antd/typography";
-import { NzEmptyModule } from "ng-zorro-antd/empty";
-import { NzSpinModule } from "ng-zorro-antd/spin";
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSpinComponent } from 'ng-zorro-antd/spin';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzTooltipModule } from 'ng-zorro-antd/tooltip';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 export const ZORRO_MODULES = [
   NzAlertModule,
@@ -68,6 +68,5 @@ export const ZORRO_MODULES = [
   NzTooltipModule,
   NzTypographyModule,
   NzSpinComponent,
-  NzSelectModule,
-  NzEmptyModule
+  NzEmptyModule,
 ];
